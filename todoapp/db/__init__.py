81f707bcf730f9441aa02d0be6deedc99d6082b9
vamsijavvadi7 <59219320+vamsijavvadi7@users.noreply.github.com>
2021-07-02 22:04:50 +0530

    Delete todoapp/db directory

--- todoapp/db/__init__.py
+++
@@ -1,8 +0,0 @@
-from .storage import MemoryStorage,JSONStorage,Storage
-from .document import Document
-
-from .db import DB
-from .table import Table
-from .query import Query
-
-__all__= ("DB","JSONStorage","Storage","MemoryStorage","Document","Table","Query")
